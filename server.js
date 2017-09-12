d0d64bbd147a0dfa1c6cd4f74aa7f71e4f51b369
The Whale <glittle@gmail.com>
2017-09-11 19:16:28 -0700

    blah

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
 
-console.log('version 1008')
+var each = function (o, cb) {
+  if (o instanceof Array) {
+    for (var i = 0; i < o.length; i++) {
+      if (cb(o[i], i, o) == false)
+        return false
+    }
+  } else {
+    for (var k in o) {
+      if (o.hasOwnProperty(k)) {
+        if (cb(o[k], k, o) == false)
+          return false
+      }
+    }
+  }
+  return true
+}
+
+console.log('version 1009')
 
 var diff_lib = require('./diff_lib.js')
 
