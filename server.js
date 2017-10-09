e78ea76cc9e7fa376c7257baebef264076ca8ff4
khermet <30908190+khermet@users.noreply.github.com>
2017-10-09 16:37:58 -0700

    Update server.js
    Bloop, yeah Bloop....

--- server.js
+++ server.js
@@ -19,12 +19,14 @@ var each = function (o, cb) {
 console.log('version 1013')
 
 var diff_lib = require('./diff_lib.js')
-var bus = require('statebus')(); bus.file_store()
+var bus = require('statebus')(); bus.sqlite_store()
+
+var db = {}
+each(bus.cache, function(value, key){
+    db[key]=value
+})
 
 var server_uid = ('U' + Math.random()).replace(/\./, '')
-
-var channel_versions = bus.fetch('channel_versions')
-var users            = bus.fetch('users')
 
 var fs = require('fs')
 var web_server = null
@@ -51,18 +53,23 @@ wss.on('connection', function connection(ws) {
         var o = JSON.parse(message)
         if (o.join) {
             uid = o.join.uid
-            if (!users[uid]) {
-                users[uid] = {}
-                users[uid].channel = o.join.channel
-                users[uid].Z = diff_lib.create_diffsyncZX2()
-                diff_lib.diffsyncZX2_commit(users[uid].Z, channel_versions[users[uid].channel] || '', server_uid)
+            var key = 'users/'+uid
+            if (!db[key]) {
+                db[key] = {}
+                db[key].channel = o.join.channel
+                db[key].id = uid
+                db[key].key = key
+                db[key].Z = diff_lib.create_diffsyncZX2()
+                var x = db['channel_versions/'+db[key].channel]
+                diff_lib.diffsyncZX2_commit(db[key].Z, (x && x.value) || '', server_uid)
             }
 
             sockets[uid] = ws
 
             try {
-                ws.send(JSON.stringify({ versions : diff_lib.diffsyncZX2_my_newish_commits(users[uid].Z, server_uid), welcome : true }))
+                ws.send(JSON.stringify({ versions : diff_lib.diffsyncZX2_my_newish_commits(db[key].Z, server_uid), welcome : true }))
             } catch (e) {}
+            bus.save(db[key])
         }
         if (o.versions) merge(o.versions)
         if (o.range) on_range(o.range)
@@ -72,9 +79,9 @@ wss.on('connection', function connection(ws) {
             } catch (e) {}
         }
         if (o.close) {
-            delete users[uid]
+            var key = 'users/'+uid
+            bus.delete(key)
         }
-        bus.save(users)
     })
     ws.on('close', () => {
         delete sockets[uid]
@@ -82,19 +89,26 @@ wss.on('connection', function connection(ws) {
     function merge(new_vs) {
         if (!new_vs) return;
         if (!uid) return;
-        diff_lib.diffsyncZX2_merge(users[uid].Z, new_vs, server_uid)
-        channel_versions[users[uid].channel] = users[uid].Z.parents_text
-        each(users, function (u, id) {
+        var user_key = 'users/'+uid
+        diff_lib.diffsyncZX2_merge(db[user_key].Z, new_vs, server_uid)
+        var key = 'channel_versions/'+db[user_key].channel
+        db[key] = {key:key, value:db[user_key].Z.parents_text}
+        each(db, function (v, k) {
+            if (!k.startsWith('users/')){return}
+            var u = v
+            var id = u.id
             if (u.channel == users[uid].channel && id != uid) {
-                var c = diff_lib.diffsyncZX2_commit(u.Z, channel_versions[users[uid].channel], server_uid)
+                var c = diff_lib.diffsyncZX2_commit(u.Z, db[key], server_uid)
                 if (c) {
                     try {
                         sockets[id].send(JSON.stringify({ versions : c }))
                     } catch (e) {}
+                    bus.save(u)
                 }
             }
         })
-        bus.save(channel_versions)
+        bus.save(db[user_key])
+        bus.save(db[key])
     }
 
     function on_range(r) {
