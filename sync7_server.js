beb466f919dc12343d7fc655befdd05b42e05a72
dglittle <glittle@gmail.com>
2018-09-25 16:44:41 -0700

    Update sync7_server.js

--- sync7_server.js
+++ sync7_server.js
@@ -1,1 +1,26 @@
-bloop!
+
+var sync7 = require('./sync7.js')
+console.log('sync7 version ' + sync7.version)
+
+var fs = require('fs')
+var web_server = null
+var server_type = null
+if (fs.existsSync('privkey.pem') && fs.existsSync('fullchain.pem')) {
+    web_server = require('https').createServer({
+        key : fs.readFileSync('privkey.pem'),
+        cert : fs.readFileSync('fullchain.pem')
+    })
+    server_type = 'https'
+} else {
+    web_server = require('http').createServer()
+    server_type = 'http'
+}
+var port = sync7.port
+web_server.listen(port)
+console.log('openning ' + server_type + ' server on port ' + port)
+var WebSocket = require('ws')
+var wss = new WebSocket.Server({ server : web_server })
+
+diffsync.create_server({
+    wss : wss
+})
